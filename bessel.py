ce673f39668c4848f1fec080502a9ddd117fd52a
Gaugain <gabrielthahand@gmail.com>
2021-01-25 22:03:01 +0100

    ajout fonction de Bessel

---
+++ bessel.py
@@ -0,0 +1,79 @@
+import scipy.special as spe
+
+
+
+### hankel's functions 
+def hn1(n,x):
+    """
+    Computation of spherical Bessel's function of the third kind or spherical Hankel's function
+    using scipy spherical Bessel's function of the first and second kind.
+
+    Params : 
+    --------
+                n   : integer - order of the function
+                x   : real / array   - param(s) of the function 
+    Returns :
+    ---------
+                output : evaluation of the function in x point(s)
+
+    """
+    return spe.spherical_jn(n,x) + 1j*spe.spherical_yn(n,x)
+
+
+def d_hn1(n,x):
+    """
+    Computation of spherical Bessel's function of the third kind derivative or spherical Hankel's function derivative
+    using scipy spherical Bessel's function of the first and second kind.
+
+    Params : 
+    --------
+                n   : integer - order of the function
+                x   : real / array   - param(s) of the function 
+    Returns :
+    ---------
+                output : evaluation of the function in x point(s)
+
+    """
+    return spe.spherical_jn(n,x,derivative =True) + 1j*spe.spherical_yn(n,x,derivative =True)
+
+
+# just for notations
+def d_jn1(n,x):
+
+    return spe.spherical_jn(n,x,derivative =True) 
+
+
+### riccati bessel functions
+def rh_n1(n,x):
+
+    return x*hn1(n,x)
+
+def d_rh_n1(n,x):
+
+    return hn1(n,x) + x*d_hn1(n,x)
+
+
+
+
+if __name__=="__main__":
+    
+    print("->    little test !!!!\n")
+    print("h01(3)")
+    print(hn1(0,3))
+    print("---------------\n")
+
+    print("d_h01(3)")
+    print(d_hn1(0,3))
+    print("---------------\n")
+
+    print("rh_01(3)")
+    print(rh_n1(0,3))
+    print("---------------\n")
+
+    print("d_rh_01(3)")
+    print(d_rh_n1(0,3))
+    print("---------------\n")
+
+
+
+
---
+++ coeff_int.py
@@ -0,0 +1,26 @@
+import scipy.special as spe
+import numpy as np
+
+def calc_coeff_int(k_int, k_ext, a):
+    """
+    Calculate the internal field coefficient c_n and d_n (from Absorption an scattering of light by small particle, C. F. ohren, D. R. Huffman)
+
+    Params :
+    --------
+                k_int : real | wave vector of the internal medium
+                k_ext : real | wave vector of the external medium
+                a     : real | radius of the sphere
+
+
+    Returns :
+    ---------
+
+                c_n :   real | coefficient for the ...
+                d_n :   real | coefficient for the ...
+
+
+    """
+
+
+
+    return 
