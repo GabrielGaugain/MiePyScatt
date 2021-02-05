71cac1898aa9cfb462b5f978954923fb3b0ee087
Gabriel GAUGAIN <ggaugain@ietr.univ-rennes1.fr>
2021-02-05 14:14:28 +0100

    cg

--- Mie_field_calculation.py
+++ Mie_field_calculation.py
@@ -18,12 +18,15 @@ def E_internal(r, theta, phi, k_int, k_ext, a, E_0 = 1, N=10):
 
     ## calculation of the associated legendre polynomials of the first order
     ## from degre 0 to N, and their first derivative (tau_n)
+
+    assert (np.size(r)==np.size(theta))&(np.size(theta)==np.size(phi)) , "r, phi and theta should have the same size !!!"
+
     pi_n, tau_n = spe.lpmn(1,N, np.cos(theta))
 
     pi_n = pi_n/np.sin(theta)     
 
 
-    E_i = np.zeros( np.size(r), np.size(theta), np.size(phi) )
+    E_i = np.zeros( (np.size(r), 3) )
 
     for n in range(1,N+1):
 
@@ -31,8 +34,8 @@ def E_internal(r, theta, phi, k_int, k_ext, a, E_0 = 1, N=10):
 
         c_n, d_n = calc_coeff_int(n, k_int, k_ext, a)
 
-        E_i = E_i + c_n * M_o1n(n, 1, k_int*r, theta, phi, pi_n, tau_n)  \
-                  + d_n * N_e1n(n, 1, k_int*r, theta, phi, pi_n, tau_n) 
+        E_i = E_i + E_n* ( c_n * M_o1n(n, 1, k_int*r, theta, phi, pi_n, tau_n)  \
+                  +        d_n * N_e1n(n, 1, k_int*r, theta, phi, pi_n, tau_n) )
 
         ##end for 
 
@@ -43,12 +46,15 @@ def E_scattered(r, theta, phi, k_int, k_ext, a, E_0 = 1, N=10):
 
     ## calculation of the associated legendre polynomials of the first order
     ## from degre 0 to N, and their first derivative (tau_n)
+
+    assert (np.size(r)==np.size(theta))&(np.size(theta)==np.size(phi)) , "r, phi and theta should have the same size !!!"
+   
     pi_n, tau_n = spe.lpmn(1,N, np.cos(theta))
     
     pi_n = pi_n/np.sin(theta)     
 
 
-    E_s = np.zeros( np.size(r), np.size(theta), np.size(phi) )
+    E_s = np.zeros( (np.size(r),3) )
 
     for n in range(1,N+1):
 
@@ -56,8 +62,8 @@ def E_scattered(r, theta, phi, k_int, k_ext, a, E_0 = 1, N=10):
 
         a_n, b_n = calc_coeff_scat(n, k_int, k_ext, a)
 
-        E_s = E_s + a_n * M_o1n(n, 3, k_int*r, theta, phi, pi_n, tau_n)  \
-                  + b_n * N_e1n(n, 3, k_int*r, theta, phi, pi_n, tau_n)        
+        E_s = E_s  + E_n * ( a_n * M_o1n(n, 3, k_int*r, theta, phi, pi_n, tau_n)    \
+                   +         b_n * N_e1n(n, 3, k_int*r, theta, phi, pi_n, tau_n)  )
 
         ##end for 
 
