c5fb5e309108d04b6e3cf86e0b938e3bf6a964a4
Gabriel GAUGAIN <ggaugain@ietr.univ-rennes1.fr>
2021-04-30 17:04:04 +0200

    added plot tio visualize the first results

--- muscle_head_calc.py
+++ muscle_head_calc.py
@@ -8,7 +8,7 @@ import coordinates as coor
 def main():
 
     ## frequency(ies) of interest
-    f = 10**6
+    f = 0.39035*10**9
     lmda = ctes.c / f
 
     n_ext = 1                   # outer refrective index : 1 for the air
@@ -19,13 +19,16 @@ def main():
     perm, cond = diel.get_diel_properties(tissue, freqs=f)
 
     n_int = np.sqrt(perm)   ## without loss => no cmplx part contrib
+    print("n_int : " ,n_int)
+    
     k_int = 2*np.pi/lmda * n_int
     k_int = k_int[0,0]
+    print("kint : ", k_int)
 
     #####################################
     a = 0.1                             # 20 cm , radius of the sphere
     L_x, L_y, L_z = 0.4, 0.4, 0.4       # size of the grid on which to compute the fields
-    N_x, N_y, N_z = 100,100,100         # number of points in each direction of the grid 
+    N_x, N_y, N_z = 150,150,150         # number of points in each direction of the grid 
 
     ## grid (in cartesian coordinates)
     ## 3D too computationnaly expensive 
@@ -44,7 +47,18 @@ def main():
 
     E = mie.calc_E_field([x,y,z], k_int, k_ext, a)    
     
+    Ex = E[:,0].reshape(X[0].shape)
+    Ey = E[:,1].reshape(X[0].shape)
+    Ez = E[:,2].reshape(X[0].shape)
+    
+    fig, axs = plt.subplots(1,3, sharex=True, sharey=True )
+    axs[0].pcolormesh(X[0],X[1], np.abs(Ex))
+    axs[1].pcolormesh(X[0],X[1], np.abs(Ey))
+    axs[2].pcolormesh(X[0],X[1], np.abs(Ez))
+    axs[0].set_xlabel("x (cm)")
+    axs[0].set_ylabel("y (cm)")
 
+    plt.show()    
 
     
     ##end main
